e6eedaed893cae79998af60816d3eca943c5213b
Henk Borghols <henk@myonlinestore.com>
2023-12-07 14:11:15 +0100

    Day 7 Part 2

--- day7/day7.php
+++ day7/day7.php
@@ -1,6 +1,6 @@
 <?php
     $input = file_get_contents('input.txt'); //Get the file
-    // $input = file_get_contents('test2.txt'); //Get the file
+    // $input = file_get_contents('test1.txt'); //Get the file
     $rows = explode("\n", $input); //Split the file by each block
 
     function getHandType($cards){
@@ -11,9 +11,7 @@
             $highestOccuringCard = chr(array_keys($counts)[1]);
         }
         $newCards = str_replace("J", $highestOccuringCard, $cards);
-        echo "$cards is now $newCards \n";
         $uniqueCards = (int)strlen(count_chars($newCards, 3));
-        echo "and it has $uniqueCards unique cards \n";
         
         switch($uniqueCards){
             case 1:
@@ -59,17 +57,14 @@
     function compareHands($hand1, $hand2){
         // $cardStrength = ['A'=>12, 'K'=>11, 'Q'=>10, 'J'=>9, 'T'=>8, '9'=>7, '8'=>6, '7'=>5, '6'=>4, '5'=>3, '4'=>2, '3'=>1, '2'=>0]; // Part 1
         $cardStrength = ['A'=>12, 'K'=>11, 'Q'=>10, 'T'=>9, '9'=>8, '8'=>7, '7'=>6, '6'=>5, '5'=>4, '4'=>3, '3'=>2, '2'=>1, 'J'=> 0];
-        // echo "Comparing $hand1 to $hand2 \n";
 
         for ($i = 0; $i < strlen($hand1); $i++) {
             $rankHand1 = $cardStrength[$hand1[$i]];
             $rankHand2 = $cardStrength[$hand2[$i]];
 
             if($rankHand1 > $rankHand2){
-                // echo "$rankHand1 > $rankHand2 dus hand 1 wint \n";
                 return 1;
             } else if($rankHand2 > $rankHand1){
-                // echo "$rankHand2 > $rankHand1 dus hand 2 wint \n";
                 return -1;
             }
         }
@@ -92,7 +87,6 @@
         foreach ($rows as $row){
             list($cards, $bet) = explode(" ", $row);
             $type = getHandType($cards);
-            // echo "Hand $cards with type $type has bet: $bet \n";
             array_push($hands, ["hand"=>$cards, "type"=>$type, "bet"=>$bet]);
         }
 
@@ -112,16 +106,13 @@
         foreach ($rows as $row){
             list($cards, $bet) = explode(" ", $row);
             $type = getHandType($cards);
-            echo "Hand $cards with type $type has bet: $bet \n";
             array_push($hands, ["hand"=>$cards, "type"=>$type, "bet"=>$bet]);
         }
 
         usort($hands, 'sortHands');
 
         foreach($hands as $i=>$hand){
-            print_r($hand);
             $winnings = $winnings + (($i + 1) * $hand["bet"]);
-            echo "i: $i, Winnings: $winnings \n\n";
         }
 
         return $winnings;
