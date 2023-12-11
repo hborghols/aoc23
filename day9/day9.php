619377ebfc1dfd87e55f5d1560a5ee87ea6f2f28
Henk Borghols <henk@myonlinestore.com>
2023-12-11 16:08:53 +0100

    Day 9 Clean

--- day9/day9.php
+++ day9/day9.php
@@ -1,9 +1,8 @@
 <?php
-    // $input = file_get_contents('input.txt'); //Get the file
-    $input = file_get_contents('test1.txt'); //Get the file
+    $input = file_get_contents('input.txt'); //Get the file
+    // $input = file_get_contents('test1.txt'); //Get the file
 
     function getDiffs($currentSequence, $sequences){
-        echo "currentSequence is: " . json_encode($currentSequence) . "\n";
         $diffSequence = [];
         $lagerThanZero = false;
 
@@ -19,14 +18,11 @@
         if ($lagerThanZero){
             return getDiffs($diffSequence, $sequences);
         } else {
-            echo "Een de diffSequence " . json_encode($diffSequence) . " \n";
             return $sequences;
         }
     }
 
     function findNextHistoryValue($sequences){
-        $nSequences = sizeof($sequences);
-
         $nextValue = 0;
 
         for ($i = sizeof($sequences) - 2; $i >= 0; $i--){
@@ -34,15 +30,24 @@
             $lastValue = (int)$sequences[$i+1][array_key_last($sequences[$i+1])];
             
             $nextValue = $currentValue + $lastValue;
-            if(!isset($lastValue)){
-                echo "No last value found! " . json_encode($sequences) . " with length " . sizeof($sequences[$i]) . "\n";
-            }
-            // echo "The next value of " . json_encode($sequences[$i]) . " is $currentValue + $lastValue = $nextValue" . "\n";
             array_push($sequences[$i], $nextValue);
         }
 
-        echo "Na " . $nSequences . " te hebben bekeken is de nextValue " . $nextValue . " \n";
         return $nextValue;
+    }
+
+    function findPreviousHistoryValue($sequences){
+        $previousValue = 0;
+
+        for ($i = sizeof($sequences) - 2; $i >= 0; $i--){
+            $currentValue = (int)$sequences[$i][array_key_first($sequences[$i])];
+            $firstValue = (int)$sequences[$i+1][array_key_first($sequences[$i+1])];
+            
+            $previousValue = $currentValue - $firstValue;
+            array_unshift($sequences[$i], $previousValue);
+        }
+
+        return $previousValue;
     }
 
     function part1($input) {
@@ -50,22 +55,29 @@
         $sum = 0;
 
         foreach($rows as $row){
-            $matches = explode(' ', $row);
-            $history = $matches;
-            // print json_encode($history) . "\n";
+            $history = explode(' ', $row);
             $sequences = getDiffs($history , [$history]);
             $nextValue = findNextHistoryValue($sequences);
             $sum += $nextValue;
-            print "The next value is $nextValue, making the sum $sum \n";
         }
 
         return $sum;
     }
 
     function part2($input) {
-        //do stuff
+        $rows = explode("\n", $input);
+        $sum = 0;
+
+        foreach($rows as $row){
+            $history = explode(' ', $row);
+            $sequences = getDiffs($history , [$history]);
+            $previousValue = findPreviousHistoryValue($sequences);
+            $sum += $previousValue;
+        }
+
+        return $sum;
     }
 
-    $result = part1($input);
+    $result = part2($input);
     echo "result: $result \n";
 ?>
